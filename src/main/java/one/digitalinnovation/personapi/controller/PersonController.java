082065e628dbed6266ebfc8a522b31ef6bef63d0
Wesley Moraes <wesleyjava88@gmail.com>
2021-09-09 21:56:39 -0300

    Get Creater

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,9 +1,12 @@
 package one.digitalinnovation.personapi.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +34,11 @@ public class PersonController {
 	public MessageResponseDTO createPerson(@Valid @RequestBody PersonDTO personDto) {
 		return personService.createPerson(personDto);
 
+	}
+	
+	@GetMapping
+	public List<PersonDTO> listAll(){
+		return personService.listAll();
 	}
 
 }
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,5 +1,8 @@
 package one.digitalinnovation.personapi.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import one.digitalinnovation.personapi.entity.Person;
 import one.digitalinnovation.personapi.mapper.PersonMapper;
@@ -20,6 +23,14 @@ public class PersonService {
 
 		Person savedPerson = personRepository.save(personToSave);
 		return MessageResponseDTO.builder().message("Created person with Id" + savedPerson.getId()).build();
+	}
+
+	public List<PersonDTO> listAll() {
+		List<Person> allPeople = personRepository.findAll();
+		
+		return allPeople.stream().
+				map(personMapper::toDTO).
+				collect(Collectors.toList());
 	}
 
 }
